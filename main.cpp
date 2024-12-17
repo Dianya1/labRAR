04b81a331dcc099b3ee8a4bc573f545cf8a8c5bd
Dianya1 <dianabashegurova@gmail.com>
2024-12-17 18:13:16 +0500

    Функция для чтения оглавления архива

--- main.cpp
+++ main.cpp
@@ -26,4 +26,29 @@ uint8_t Method;
 uint16_t NameSize;
 uint32_t FileAttr;
 };
+#pragma pack(pop)
 
+void readheaderRar(vector<char>& rar_data){
+    header_format* p_header = reinterpret_cast<header_format*>(&rar_data[7]);
+    int offset = 7 + int(p_header -> header_size);
+    while (true){
+        header_format* p_header = reinterpret_cast <header_format*> (&rar_data[offset]);
+        file_head* f_header = reinterpret_cast <file_head*> (&rar_data[offset+7]);
+        if (p_header -> header_type == 0x74)
+        {
+            uint16_t sizeHeader = f_header -> NameSize;
+            uint16_t offsetHeader = offset + sizeof (struct header_format) + sizeof (struct file_head);
+            if (offsetHeader + sizeHeader > rar_data.size())
+                break;
+            char* jumpCell = reinterpret_cast<char*>(&rar_data[offsetHeader]);
+            string utf8line(jumpCell, sizeHeader);
+            vector <wchar_t> utf16content(sizeHeader);
+            MultiByteToWideChar(CP_UTF8,0,utf8line.c_str(),sizeHeader,utf16content.data(),utf16content.size());
+            wstring FileName(utf16content.begin(),utf16content.end());
+            wcout << "File name: " << FileName << endl;
+            offset = offset + p_header -> header_size + f_header -> PackSize;
+        }
+        else
+            offset += p_header->header_size;
+    }
+};
