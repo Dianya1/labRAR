e84a41e1abf50b53c8a9ed8675c36aa579280d23
Dianya1 <dianabashegurova@gmail.com>
2024-12-17 18:07:54 +0500

    Обявлена структура2

--- main.cpp
+++ main.cpp
@@ -15,4 +15,15 @@ uint16_t header_flags;
 uint16_t header_size;
 };
 
+struct file_head{
+uint32_t PackSize;
+uint32_t UnpSize;
+uint8_t HostOS;
+uint32_t FileCRC;
+uint32_t FileTime;
+uint8_t UnpVer;
+uint8_t Method;
+uint16_t NameSize;
+uint32_t FileAttr;
+};
 
